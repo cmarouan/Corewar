0f487d8e1b8cac1aea1a950e7619a59dbafcf7cb
cmarouan <merouan.chakour@gmail.com>
2019-11-03 16:47:10 +0100

    norme of ft_check and ft_exec

--- vm/srcs/ft_check.c
+++ vm/srcs/ft_check.c
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmarouan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/03 16:37:02 by cmarouan          #+#    #+#             */
+/*   Updated: 2019/11/03 16:38:03 by cmarouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "corewar.h"
 
-void ft_check(t_vm *vm)
+void	ft_check(t_vm *vm)
 {
-	vm->nbr_of_check++;
+	int	i;
 
+	i = 0;
+	vm->nbr_of_check++;
 	if (vm->nbr_live >= NBR_LIVE)
 	{
 		vm->cycle_to_die -= CYCLE_DELTA;
@@ -16,16 +29,12 @@ void ft_check(t_vm *vm)
 		vm->nbr_of_check = 0;
 	}
 	vm->nbr_live = 0;
-	//ft_printf("start %d\n",vm->cycle_from_start);
-	int i = 0;
 	while (i < vm->player_c)
 	{
 		vm->players[i].live_in_current_period = 0;
 		i++;
 	}
-	
 	vm->process = ft_kill_process(vm);
-	
 	if (vm->f_vus)
-        wrefresh(vm->w_info);
+		wrefresh(vm->w_info);
 }
--- vm/srcs/ft_exec.c
+++ vm/srcs/ft_exec.c
@@ -1,50 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exec.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmarouan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/03 16:43:06 by cmarouan          #+#    #+#             */
+/*   Updated: 2019/11/03 16:43:52 by cmarouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "corewar.h"
 
-void ft_exec(t_process *p, t_vm *vm)
+void	ft_exec(t_process *p, t_vm *vm)
 {
-
 	if (p->cycle_to_wait <= -1)
 	{
-	//	ft_printf("code %p \n", p->pc);
-		
-		
 		if (p->pc->byte > 0 && p->pc->byte <= 16)
 		{
 			p->opcode = p->pc->byte;
 			p->cycle_to_wait = op_tab[p->opcode - 1].cycle - 1;
-			//ft_printf("%s\n", op_tab[p->opcode - 1].name);
 		}
 		else
 			PC_INCR(vm, p, 1);
-
-
-		
 		if (p->oldindex == -1)
 			ft_move_pc(p, vm);
-		
 	}
-    else if (p->cycle_to_wait == 0)
+	else if (p->cycle_to_wait == 0)
 	{
-		
-		//ft_printf("exec on Cycle  %d : \n",vm->cycle_from_start);	
-		// Temp instruvtion : ????/
-		//if (p->opcode == 0x0b) p->opcode = 2;
-		// Temp instruction : ????/
-		
 		if (p->opcode >= 1 && p->opcode <= 16)
-		{
-			//ft_printf("cycle %d Oper  %s\n",vm->cycle_from_start, op_tab[p->opcode - 1].name);
-			// if (vm->cycle_from_start >= 5440 && vm->cycle_from_start <= 5450)
-			// 	ft_printf("op name : %s\n", op_tab[p->pc->byte - 1].name);
 			vm->instruction[p->opcode - 1](vm, p);
-			
-		}
-		// Temp instruction : ????/
 		else
 			PC_INCR(vm, p, 1);
-		
-		//p->cycle_to_wait = -1;
-		// Temp instruction : ????/
-		//exit(0);
 	}
 }
