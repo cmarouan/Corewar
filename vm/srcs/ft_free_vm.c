10a89a7d9b7bf634b0478bada2c5602903676eed
cmarouan <merouan.chakour@gmail.com>
2019-11-03 16:49:25 +0100

    free_vm normed

--- vm/srcs/ft_free_vm.c
+++ vm/srcs/ft_free_vm.c
@@ -1,14 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free_vm.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmarouan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/03 16:47:38 by cmarouan          #+#    #+#             */
+/*   Updated: 2019/11/03 16:48:54 by cmarouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "corewar.h"
 
-void ft_free_vm(t_vm *vm)
+void	ft_free_vm(t_vm *vm)
 {
-	t_process *tmp = vm->process;
-	t_process *prev;
+	t_process	*tmp;
+	t_process	*prev;
+	int			i;
 
-	int i = 0;
+	i = 0;
+	tmp = vm->process;
 	while (i < vm->player_c)
 	{
-		//free(vm->players[i].file_name);
 		free(vm->players[i].code);
 		i++;
 	}
@@ -21,7 +34,6 @@ void ft_free_vm(t_vm *vm)
 		free(prev->reg);
 		free(prev);
 	}
-	//free(vm->instruction[0]);
 	free(vm->memory);
 	free(vm);
 }
