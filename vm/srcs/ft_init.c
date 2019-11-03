6806478002bd0bd755235cc1529b6130e5a7f78e
Khalid Moussaid <kmoussai@e1r9p7.1337.ma>
2019-11-03 23:15:10 +0100

    norm start and init

--- vm/srcs/ft_init.c
+++ vm/srcs/ft_init.c
@@ -1,42 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_init.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmoussai <kmoussai@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/03 22:48:04 by kmoussai          #+#    #+#             */
+/*   Updated: 2019/11/03 22:52:36 by kmoussai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "corewar.h"
 
-t_vm	*ft_init_vm(void)
+static void	ft_init_player(t_vm *vm)
 {
-	t_vm *vm;
-	
-	
-	vm = (t_vm *)malloc(sizeof(t_vm));
-	ft_memset(vm, 0, sizeof(vm));
-	vm->win = 0;
-	vm->aff = 0;
-	vm->f_dump = -1;
-	vm->f_show = -1;
-	vm->f_vus = 0;
-	vm->f_log = 0;
+	int i;
+
+	i = 0;
 	vm->player_c = 0;
-	vm->process = NULL;
-	vm->pc_count = 0;
-	vm->nbr_live = 0;
-	vm->current_cycle = 0;
-	vm->cycle_to_die = CYCLE_TO_DIE;
-	vm->cycle_from_start = 0;
-	vm->memory = NULL;
-	vm->nbr_of_check = 0;
-	vm->speed = 1;
-	vm->pause = 1;
-	vm->pc_ids = 0;
-	vm->pc_count = 0;
 	vm->winner = 0;
-	int i = 0;
-	vm->w_memory = NULL;
-	vm->w_info = NULL;
+	vm->nbr_of_check = 0;
 	vm->ids = (int *)malloc(sizeof(int) * (MAX_PLAYERS + 1));
 	ft_bzero(vm->ids, sizeof(int) * (MAX_PLAYERS + 1));
 	vm->players = (t_player *)malloc(sizeof(t_player) * MAX_PLAYERS);
 	while (i < MAX_PLAYERS)
 	{
-		
 		vm->players[i].id_set = 0;
 		vm->players[i].id = -1;
 		vm->players[i].file_name = NULL;
@@ -47,42 +35,58 @@ t_vm	*ft_init_vm(void)
 		vm->players[i].code = NULL;
 		i++;
 	}
+}
+
+t_vm		*ft_init_vm(void)
+{
+	t_vm *vm;
+
+	vm = (t_vm *)malloc(sizeof(t_vm));
+	ft_memset(vm, 0, sizeof(vm));
+	vm->win = 0;
+	vm->aff = 0;
+	vm->f_dump = -1;
+	vm->f_show = -1;
+	vm->f_vus = 0;
+	vm->f_log = 0;
+	vm->process = NULL;
+	vm->pc_count = 0;
+	vm->nbr_live = 0;
+	vm->current_cycle = 0;
+	vm->cycle_to_die = CYCLE_TO_DIE;
+	vm->cycle_from_start = 0;
+	vm->memory = NULL;
+	vm->speed = 1;
+	vm->pause = 1;
+	vm->pc_ids = 0;
+	vm->pc_count = 0;
+	vm->w_memory = NULL;
+	vm->w_info = NULL;
+	ft_init_player(vm);
 	return (vm);
 }
 
-
-void ft_init_memory(t_vm *vm)
+void		ft_init_memory(t_vm *vm)
 {
-	int i = -1;
+	int i;
+	int j;
+	int h;
+
 	vm->memory = (t_memory *)malloc(sizeof(t_memory) * MEM_SIZE);
 	ft_bzero(vm->memory, sizeof(t_memory) * MEM_SIZE);
-	// while (++i < MEM_SIZE)
-	// {
-	// 	vm->memory[i].byte = 0;
-	// 	vm->memory[i].p_id = -1;
-	// }
-	
-	int j;
 	i = 0;
 	while (i < vm->player_c)
 	{
-		j = (MEM_SIZE/vm->player_c)*i;
-		int h = 0;
+		j = (MEM_SIZE / vm->player_c) * i;
+		h = 0;
 		vm->process = ft_add_pc(vm, MOD(j), &vm->players[i]);
-		//ft_printf("player code : %#p %d\n", &vm->players[i].code[h], h);
-		//
 		while (h < vm->players[i].prog_size)
 		{
-			
-			vm->memory[j%MEM_SIZE].byte = vm->players[i].code[h];
-			vm->memory[j%MEM_SIZE].p_id = vm->players[i].id;
-			//exit(0);
+			vm->memory[j % MEM_SIZE].byte = vm->players[i].code[h];
+			vm->memory[j % MEM_SIZE].p_id = vm->players[i].id;
 			h++;
 			j++;
 		}
-	//	free(vm->players[i].code);
 		i++;
 	}
-	//ft_printf("%#p\n", &vm->players[i].code[0]);
-
 }
--- vm/srcs/ft_start.c
+++ vm/srcs/ft_start.c
@@ -1,55 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_start.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmoussai <kmoussai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/03 22:53:41 by kmoussai          #+#    #+#             */
+/*   Updated: 2019/11/03 23:09:32 by kmoussai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "corewar.h"
 
-
-int ft_start(t_vm *vm)
+static void	ft_check_vm(t_vm *vm)
 {
-	//int ret;
-	
-	vm->cycle_from_start = 0;
+	if (vm->current_cycle == 1)
+	{
+		ft_check(vm);
+		if (vm->f_vus)
+			window_right(vm->w_info, vm);
+		vm->current_cycle = vm->cycle_to_die;
+	}
+	else
+		vm->current_cycle--;
+}
+
+static void	ft_exec_process(t_vm *vm)
+{
+	t_process *tmp;
+
+	tmp = vm->process;
+	while (tmp)
+	{
+		if (tmp->cycle_to_wait <= 0)
+			ft_exec(tmp, vm);
+		if (tmp->cycle_to_wait > -1)
+			tmp->cycle_to_wait--;
+		if (vm->f_vus)
+			window_right(vm->w_info, vm);
+		tmp = tmp->next;
+	}
+	ft_check_vm(vm);
+}
+
+static int	ft_vis_control(t_vm *vm)
+{
+	if (vm->f_vus)
+	{
+		if (ft_event_handler(vm, wgetch(vm->w_info)) == -1)
+			return (1);
+		window_right(vm->w_info, vm);
+		wrefresh(vm->w_memory);
+		usleep(SECOND / vm->speed);
+	}
+	return (0);
+}
+
+int			ft_start(t_vm *vm)
+{
+	char c;
+
 	vm->current_cycle = vm->cycle_to_die;
 	while (vm->current_cycle > 0)
 	{
-		if (vm->f_vus)
-		{
-			//wgetch(vm->w_info);
-			if (ft_event_handler(vm, wgetch(vm->w_info)) == -1)
-			  	break;
-			window_right(vm->w_info, vm);
-			wrefresh(vm->w_memory);
-			usleep(SECOND / vm->speed);
-		}
-		t_process *tmp = vm->process;
+		if (ft_vis_control(vm))
+			break ;
 		vm->cycle_from_start++;
-		while (tmp)
-		{
-				if (tmp->cycle_to_wait <= 0)
-					ft_exec(tmp, vm);
-				if (tmp->cycle_to_wait > -1)
-					tmp->cycle_to_wait--;
-				if (vm->f_vus)
-					window_right(vm->w_info, vm);
-			tmp = tmp->next;
-		}
-		if (vm->current_cycle == 1)
-		{
-			ft_check(vm);
-			if (vm->f_vus)
-				window_right(vm->w_info, vm);
-			vm->current_cycle = vm->cycle_to_die;
-		}else
-			vm->current_cycle--;
+		ft_exec_process(vm);
 		if (!vm->process)
-			break;
+			break ;
 		if (vm->f_dump > 0 && vm->f_dump == vm->cycle_from_start && !vm->f_vus)
 		{
 			print_mem(vm->memory);
 			ft_free_vm(vm);
 			return (-1);
 		}
-		if (vm->f_show > 0 && !(vm->cycle_from_start % vm->f_show) && !vm->f_vus)
+		if (vm->f_show > 0 && !(vm->cycle_from_start % vm->f_show)
+				&& !vm->f_vus)
 		{
 			print_mem(vm->memory);
-			char c;
 			read(0, &c, 1);
 		}
 	}
