d0c8be539940cbdaff3e2b68ba0bcdae567fd025
Khalid Moussaid <kmoussai@e1r9p7.1337.ma>
2019-11-05 12:53:56 +0100

    add champ

--- weld_nass.s
+++ weld_nass.s
@@ -1,7 +1,11 @@
-.name "Weld nass"
+.name "weld nass"
 .comment "we crashed everythings front of us"
 
+        live %15
+        fork %511
 start:
-        sti r0, %:l_live, %1
+        sti r1, %:l_live, %1
 l_live:
         live %15
+        ld %0 , r5
+        zjmp %:start
